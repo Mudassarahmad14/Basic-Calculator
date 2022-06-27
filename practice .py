e145b80cda18541d96a5b64baea705d85a649f47
Mudassar Ahmad <workmudassarahmad@gmail.com>
2022-06-27 21:50:55 +0500

    Add files via upload

---
+++ practice .py
@@ -0,0 +1,28 @@
+first = input("Enter you operator, +, -, *, /")
+number1 = input("Enter your first value:")
+number2= input ("enter your second value:")
+
+#Collecting Data above ^
+
+if first == "+":
+     print (int(number1)+int(number2))
+
+elif first == "-":
+    print (int(number1)-int(number2))
+
+elif first == "*":
+    print (int(number1)*int(number2))
+elif first == "/":
+    print (int(number1)/int(number2))
+
+else :
+
+
+
+    print ("Invalid response")
+
+
+
+
+
+    #performing actions above 
